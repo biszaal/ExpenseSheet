7d9bf7c9b37e67c641807466abdb862951ba35a0
Bishal Aryal <biszaal@yahoo.com>
2020-05-30 02:33:59 +0545

    Initial Commit

---
+++ Expense Sheet/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Expense Sheet
+//
+//  Created by Bishal Aryal on 5/30/20.
+//  Copyright © 2020 Bishal Aryal. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
